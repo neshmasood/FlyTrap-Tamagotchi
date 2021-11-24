d21ab369ab8d97bca53563ddce6af37df1e07f5d
neshmasood <neshtam@gmail.com>
2021-11-24 00:37:13 -0600

    VS code setup

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,27 @@
+const newGame = {
+    name: nameInput,
+    age: "",
 
+
+
+
+},
+
+// clickFood(){
+//     console.log("feed click")
+//     game.hunger++;
+//     $("#img-Landing-Page").addClass("shake")
+    
+//     setTimeout(function(){
+//         $("#img-Landing-Page").removeClass("shake");
+//     }, 1000);
+//     if(game.hunger >= 10) {
+//         game.hunger = 10;
+//     }
+//     $("#parentAccouncement").text(`hunger: ${game.hunger}/10`);
+    
+// },
+
+// if water>=
 
 
