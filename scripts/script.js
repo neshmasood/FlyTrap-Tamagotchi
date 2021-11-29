14792baccc3ebce879e3b9fd39078f025fcc2714
neshmasood <neshtam@gmail.com>
2021-11-29 00:56:12 -0600

    Multiple functions complete

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,109 @@
-const newGame = {
-    name: nameInput,
-    age: "",
+// Define all variables
+    let name = "input-name";
+    let health;
+    let waterAmount = 4;
+    let foodAmount =4;
+    let sunshineAmount = 4;
+    let setInterval = 3000;
+    let lowestLevel = 4; //lowest level for plant to talk
+
+    // hideImages();
+    // waterLow();
+    // foodLow();
+    // sunshineLow();
+
+
+
+function startGame(){
+    let name = document.getElementById("input-name").value;
+    document.getElementById("input-button").addEventListener("click", PLAY);
+
+}
+// Write a function to hide all images
+
+function hideImages() {
+    document.getElementById("thirsty").style.visibility = "hidden";
+    document.getElementById("hungry").style.visibility = "hidden";
+    document.getElementById("sunlight").style.visibility = "hidden";
+    document.getElementById("waterDropsImage").style.visibility = "hidden";
+    document.getElementById("flyImage").style.visibility = "hidden";
+    document.getElementById("sunImage").style.visibility = "hidden";
+
+  }
+
+
+//   Write a function to reduce value
+  function reduceValue(){
+
+    waterAmount = waterAmount - Math.floor(Math.random()*2 +1);
+
+    foodAmount = foodAmount - Math.floor(Math.random()*2 +1);
+
+    sunshineAmount = sunshineAmount - Math.floor(Math.random()*2 +1);
+  }
+
+//   Write a function for Low level of waterAmount, foodAmount, sunshineAmount
+function waterLow(){
+      if(waterAmount <= lowestLevel){
+        document.getElementById("thirsty").style.visibility = "visible";  
+      }
+  }
+
+function foodLow(){
+    if(foodAmount <= lowestLevel){
+        document.getElementById("hungry").style.visibility = "visible"; 
+    }
+}
+function sunshineLow(){
+    if(sunshineAmount <= lowestLevel){
+      document.getElementById("sunlight").style.visibility = "visible";  
+    }
+}
+
+
+
+// Write a function to change Plant images based on health value
+
+function changePlantImages(){
+    if(waterAmount =0 && (foodAmount = 0) && (sunshineAmout = 0)){
+        gameOver();
+        document.getElementsById("deadPlant").style.visibility = "visible";
+    }else if(waterAmount <=9 || waterAmount == 1){
+        document.getElementsById("babyPlant").style.visibility = "visible";
+    }
+}
+ 
+function changePlantImages(){
+    if(waterAmount =0 && (foodAmount = 0) && (sunshineAmout = 0)){
+        gameOver();
+        document.getElementsById("deadPlant").style.visibility = "visible";
+    }else if(waterAmount <=9 || waterAmount == 1){
+        document.getElementsById("babyPlant").style.visibility = "visible";
+    }
+}
+
+function changePlantImages(){
+    if(waterAmount =0 && (foodAmount = 0) && (sunshineAmout = 0)){
+        gameOver();
+        document.getElementsById("deadPlant").style.visibility = "visible";
+    }else if(waterAmount <=9 || waterAmount == 1){
+        document.getElementsById("babyPlant").style.visibility = "visible";
+    }
+}
+
+
+// Write a function for button clicks
+
+// function waterButton(){
+//     document.getElementById("drink").addEventListener("click","waterDropsImage");
+// }
 
 
 
 
-},
 
-// clickFood(){
-//     console.log("feed click")
-//     game.hunger++;
-//     $("#img-Landing-Page").addClass("shake")
-    
-//     setTimeout(function(){
-//         $("#img-Landing-Page").removeClass("shake");
-//     }, 1000);
-//     if(game.hunger >= 10) {
-//         game.hunger = 10;
-//     }
-//     $("#parentAccouncement").text(`hunger: ${game.hunger}/10`);
-    
-// },
 
-// if water>=
+
+
 
 
